4dadf710125bfcc2f85abb1901afb4f727b35726
Graham Theobald <Graham_Theobald@live.co.uk>
2022-11-12 16:39:23 +0000

    centered numbers

---
+++ script.js
@@ -0,0 +1,34 @@
+const TIMES = {
+  seconds: [1000, 'minutes'],
+  minutes: [1000 * 60, 'hours'],
+  hours: [1000 * 60 * 60, 'days'],
+  days: [1000 * 60 * 60 * 24, 'one'],
+  one: [Infinity]
+}
+
+const date = new Date(2023, 0, 18)
+const formatter = new Intl.NumberFormat('en-US', {minimumIntegerDigits: 2})
+
+const elements = document.querySelectorAll("[data-time]")
+
+function updateElements() {
+  const miliSeconds = date - new Date()
+  elements.forEach(el => {
+    const div = el.querySelector("p")
+    const timeFrame = el.dataset.time
+    const time = TIMES[timeFrame]
+    const mod = TIMES[time[1]][0]
+    const next = formatter.format(Math.floor(miliSeconds  % mod / time[0]))
+    if (next != div.innerText) {
+      div.classList.add('animate')
+      div.innerText = next
+    }
+    else {
+      div.classList.remove('animate')
+    }
+  })
+}
+
+updateElements()
+
+setInterval(() => updateElements(), 1000)
